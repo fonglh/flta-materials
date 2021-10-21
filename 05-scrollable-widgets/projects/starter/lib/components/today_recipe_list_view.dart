a9050e63e9dd470ece00e5348afcaa93945bd70d
fonglh <fonglh@gmail.com>
2021-10-22 02:09:18 +0800

    Chapter 5. Using ListViews.
    Create scrollable widgets for Explore screen.

---
+++ 05-scrollable-widgets/projects/starter/lib/components/today_recipe_list_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// Import the barrel files to use data and UI components
+import '../components/components.dart';
+import '../models/models.dart';
+
+class TodayRecipeListView extends StatelessWidget {
+  // Needs a list of recipes to display
+  final List<ExploreRecipe> recipes;
+
+  const TodayRecipeListView({
+    Key? key,
+    required this.recipes,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header text
+          Text('Recipes of the Day 🍳',
+              style: Theme.of(context).textTheme.headline1),
+          const SizedBox(height: 16),
+          // This container will hold the horizontal list view
+          Container(
+              height: 400,
+              color: Colors.transparent,
+              // 2 IndexedWidgetBuilders
+              child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: recipes.length,
+                  // itemBuilder callback will go through every item in the list
+                  itemBuilder: (context, index) {
+                    final recipe = recipes[index];
+                    return buildCard(recipe);
+                  },
+                  // separatorBuilder callback goes through every item in the
+                  // list
+                  separatorBuilder: (context, index) {
+                    // Space each item 16px apart
+                    return const SizedBox(
+                      width: 16,
+                    );
+                  })),
+        ],
+      ),
+    );
+  }
+
+  Widget buildCard(ExploreRecipe recipe) {
+    if (recipe.cardType == RecipeCardType.card1) {
+      return Card1(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card2) {
+      return Card2(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card3) {
+      return Card3(recipe: recipe);
+    } else {
+      throw Exception('This card doesn\'t exist yet');
+    }
+  }
+}
---
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  // To mock server responses
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Create FutureBuilder within widget's build
+    return FutureBuilder(
+      // getExploreData returns a future that returns an ExploreData instance.
+      // That contains 2 lists, todayRecipes and friendPosts
+      future: mockService.getExploreData(),
+      // within builder, use snapshot to check the state of the Future
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // TODO: Add Nested List Views
+        // Future is complete, can extract the data.
+        if (snapshot.connectionState == ConnectionState.done) {
+          // snapshot.data returns ExploreData, can extract todayRecipes to pass
+          // to the ListView.
+          final recipes = snapshot.data?.todayRecipes ?? [];
+
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: recipes),
+              const SizedBox(height: 16),
+              // TODO: Replace this with FriendPostListView
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
+          );
+        } else {
+          // The Future is still loading, show a spinner to let the user know
+          // that something is happening
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
