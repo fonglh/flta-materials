17c06d4ada8e23c6ae44810865eafef9ea12244e
fonglh <fonglh@gmail.com>
2021-10-28 17:42:13 +0800

    Chapter 6. Add gestures to support tap to edit, swipe to delete.

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
@@ -24,20 +24,63 @@ class GroceryListScreen extends StatelessWidget {
         itemCount: groceryItems.length,
         itemBuilder: (context, index) {
           final item = groceryItems[index];
-          // TODO 28: Wrap in a Dismissable
-          // TODO 27: Wrap in an InkWell
-          // Get the current GroceryItem to construct a GroceryTile
-          return GroceryTile(
+          return Dismissible(
+            // Flutter needs this to find and remove the right element in the
+            // tree
             key: Key(item.id),
-            item: item,
-            // Callback handler for tapping the checkbox
-            onComplete: (change) {
-              // Checks if there is a change and toggles the item's isComplete
-              // status through the manager.
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+            // Sets the direction the user can swipe to dismiss
+            direction: DismissDirection.endToStart,
+            // Widget to display behind the widget being swiped
+            // Red with a white trashcan on center right
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              child: const Icon(Icons.delete_forever,
+                  color: Colors.white, size: 50.0),
+            ),
+            // lets me know when user swiped away a grocery tile
+            onDismissed: (direction) {
+              // Pass index for grocery manager to delete the item.
+              manager.deleteItem(index);
+              // Show snack bar to inform the user what item got deleted
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${item.name} dismissed')));
             },
+            // Wrap GroceryTile in an InkWell
+            child: InkWell(
+              // Get the current GroceryItem to construct a GroceryTile
+              child: GroceryTile(
+                key: Key(item.id),
+                item: item,
+                // Callback handler for tapping the checkbox
+                onComplete: (change) {
+                  // Checks if there is a change and toggles the item's
+                  // isComplete status through the manager.
+                  if (change != null) {
+                    manager.completeItem(index, change);
+                  }
+                },
+              ),
+              // Presents GroceryItemScreen for user to edit the current item.
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => GroceryItemScreen(
+                              originalItem: item,
+                              // Called by GroceryItemScreen when user updates
+                              // an item
+                              onUpdate: (item) {
+                                // Updates item at the index
+                                manager.updateItem(item, index);
+                                // Dismisses GroceryItemScreen
+                                Navigator.pop(context);
+                              },
+                              // Not called since we're updating and item
+                              onCreate: (item) => {},
+                            )));
+              },
+            ),
           );
         },
         separatorBuilder: (context, index) {
