b114b70d64a78f3c088c12faae62d28d0b822304
fonglh <fonglh@gmail.com>
2021-11-05 00:47:31 +0800

    Chapter 7. Navigation of grocery item screens for create and update.

--- 07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
+++ 07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
@@ -60,8 +60,32 @@ class AppRouter extends RouterDelegate
         // TODO: Add Home
         if (appStateManager.isOnboardingComplete)
           Home.page(appStateManager.getSelectedTab),
-        // TODO: Create new item
-        // TODO: Select GroceryItemScreen
+        // Checks if user is creating new grocery item
+        if (groceryManager.isCreatingNewItem)
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              // Update the grocery list once the user saves the item
+              groceryManager.addItem(item);
+            },
+            onUpdate: (item, index) {
+              // This only gets called when the user updates an item.
+            },
+          ),
+
+        // Checks if a grocery item is selected
+        if (groceryManager.selectedIndex != -1)
+          GroceryItemScreen.page(
+              item: groceryManager.selectedGroceryItem,
+              index: groceryManager.selectedIndex,
+              onUpdate: (item, index) {
+                // Updates item at current index when user changes and saves
+                // the item
+                groceryManager.updateItem(item, index);
+              },
+              onCreate: (_) {
+                // This only gets called when the user adds a new item.
+              }),
+
         // TODO: Add Profile Screen
         // TODO: Add WebView Screen
       ],
@@ -89,7 +113,12 @@ class AppRouter extends RouterDelegate
       appStateManager.logout();
     }
 
-    // TODO: Handle state when user closes grocery item screen
+    // Reset appropriate state when the user taps the back button from the
+    // grocery item screen.
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+
     // TODO: Handle state when user closes profile screen
     // TODO: Handle state when user closes WebView screen
     // 6
