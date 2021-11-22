47eb309ef10e695bfe653ae1dc825802d7c4b1f2
fonglh <fonglh@gmail.com>
2021-11-22 23:50:07 +0800

    Chapter 15. Implement repository-like functions.
    Add functions to find, insert and delete recipe and ingredients data.

--- 15-saving-data-with-sqlite/projects/starter/lib/data/sqlite/database_helper.dart
+++ 15-saving-data-with-sqlite/projects/starter/lib/data/sqlite/database_helper.dart
@@ -117,6 +117,119 @@ class DatabaseHelper {
     return ingredients;
   }
 
-// TODO: Add findAppRecipes here
+  Future<List<Recipe>> findAllRecipes() async {
+    // Get the database
+    final db = await instance.streamDatabase;
+    // Use `query` to get all the recipes
+    final recipeList = await db.query(recipeTable);
+    // parse to a list of Recipes
+    final recipes = parseRecipes(recipeList);
+    return recipes;
+  }
 
+  // async* and yield* keywords are used to signal a stream
+  Stream<List<Recipe>> watchAllRecipes() async* {
+    final db = await instance.streamDatabase;
+    // `yield` creates a stream using the query
+    yield* db
+        // create a query using recipeTable
+        .createQuery(recipeTable)
+        // For each row, convert the row to a list of recipes.
+        .mapToList((row) => Recipe.fromJson(row));
+  }
+
+  // async* and yield* keywords are used to signal a stream
+  Stream<List<Ingredient>> watchAllIngredients() async* {
+    final db = await instance.streamDatabase;
+    yield* db
+        .createQuery(ingredientTable)
+        .mapToList((row) => Ingredient.fromJson(row));
+  }
+
+  Future<Recipe> findRecipeById(int id) async {
+    final db = await instance.streamDatabase;
+    // could also use `rawQuery` and write raw SQL commands
+    final recipeList = await db.query(recipeTable, where: 'id = $id');
+    final recipes = parseRecipes(recipeList);
+    return recipes.first;
+  }
+
+  Future<List<Ingredient>> findAllIngredients() async {
+    final db = await instance.streamDatabase;
+    final ingredientList = await db.query(ingredientTable);
+    final ingredients = parseIngredients(ingredientList);
+    return ingredients;
+  }
+
+  Future<List<Ingredient>> findRecipeIngredients(int recipeId) async {
+    final db = await instance.streamDatabase;
+    // use `where` clause to find ingredients belonging to a specific recipe
+    final ingredientList =
+        await db.query(ingredientTable, where: 'recipeId = $recipeId');
+    final ingredients = parseIngredients(ingredientList);
+    return ingredients;
+  }
+
+  // Take table name and JSON map
+  Future<int> insert(String table, Map<String, dynamic> row) async {
+    final db = await instance.streamDatabase;
+    // Use Sqlbrite's insert
+    return db.insert(table, row);
+  }
+
+  Future<int> insertRecipe(Recipe recipe) {
+    // Return values from `insert` using recipe's table and JSON data
+    return insert(recipeTable, recipe.toJson());
+  }
+
+  Future<int> insertIngredient(Ingredient ingredient) {
+    // Return values from `insert` using ingredient's table and JSON data
+    return insert(ingredientTable, ingredient.toJson());
+  }
+
+  // Private function to delete data from the table with the provided
+  // column and row id.
+  Future<int> _delete(String table, String columnId, int id) async {
+    final db = await instance.streamDatabase;
+    // Delete a row where columnId equals to the passed in id
+    return db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteRecipe(Recipe recipe) async {
+    if (recipe.id != null) {
+      return _delete(recipeTable, recipeId, recipe.id!);
+    } else {
+      return Future.value(-1);
+    }
+  }
+
+  Future<int> deleteIngredient(Ingredient ingredient) async {
+    if (ingredient.id != null) {
+      return _delete(ingredientTable, ingredientId, ingredient.id!);
+    } else {
+      return Future.value(-1);
+    }
+  }
+
+  Future<void> deleteIngredients(List<Ingredient> ingredients) {
+    ingredients.forEach((ingredient) {
+      if (ingredient.id != null) {
+        _delete(ingredientTable, ingredientId, ingredient.id!);
+      }
+    });
+    return Future.value();
+  }
+
+  Future<int> deleteRecipeIngredients(int id) async {
+    final db = await instance.streamDatabase;
+    // If you use whereArgs, you need to use a ? for each item in the list of
+    // arguments. Notice the last method. It uses whereArgs: [id]. This is an
+    // array of parameters. For every question mark, you need an entry in the
+    // array.
+    return db.delete(ingredientTable, where: '$recipeId = ?', whereArgs: [id]);
+  }
+
+  void close() {
+    _streamDatabase.close();
+  }
 }
